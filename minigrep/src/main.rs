5d6018f85470d9f4a5821849a78f3eeb69e00bc3
Suhird <suhirdsingh92@gmail.com>
2025-01-30 22:03:33 -0500

    w.i.p

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,5 @@
 use minigrep::read_file;
-use std::env;
+use std::{env, process};
 
 struct Config {
     query: String,
@@ -7,20 +7,22 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: Vec<String>) -> Self {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
-        Self {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     println!("Querying with:{}", config.query);
     let contents = read_file(&config.file_path).expect("Cannot read file because of error, {}");
     println!("With text:\n{contents}");
